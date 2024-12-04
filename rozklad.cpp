a8c32b396fcabd51f5803cfcbb2765eb1a61bac5
skvoznyak12 <146167099+skvoznyak12@users.noreply.github.com>
2024-12-04 09:07:38 +0100

    Add files via upload

---
+++ rozklad.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void f1(int n) {
+    int k = 2; // 
+    cout << "Czynniki pierwsze liczby " << n << ": ";
+    
+    while (n > 1) {
+        while (n % k == 0) { 
+            cout << k << " "; 
+            n /= k;          
+        }
+        k++; // Przejdź do następnego potencjalnego dzielnika
+    }
+    
+    cout << endl;
+}
+
+int main() {
+    int n;
+
+    cout << "Podaj liczbe do rozkladu na czynniki pierwsze: ";
+    cin >> n;
+
+    if (n < 2) {
+        cout << "Liczba musi być większa lub równa 2." << endl;
+    } else {
+        f1(n);
+    }
+
+    return 0;
+}
